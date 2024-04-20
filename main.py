35f9ed06a733b2d3fa99e180c92cef0b2b6f362b
itsryuku <39871872+itsryuku@users.noreply.github.com>
2024-04-20 12:27:07 -0400

    fix slow viewbotting

--- main.py
+++ main.py
@@ -4,7 +4,6 @@
 @Date: 05/03/2024
 """
 import re 
-import time
 import json
 import httpx
 import random
@@ -60,8 +59,7 @@ def extract_vid(livestream):
                         video_id = match.group(1)
     return video_id
 
-
-def generate_user_agent(n):
+async def generate_user_agent(n):
     """
     Generates user-agents to use when botting.
     Rumble links every viewer_id to the user-agent that was used to generate it, 
@@ -73,14 +71,14 @@ def generate_user_agent(n):
     Returns:
         list: A list of generated user-agents.
     """
+    camouflage = "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.6367.54 Mobile Safari/537.36"
     user_agents = []
     for _ in range(n):
-        me = "Ryuku-was-here"
         rand = ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
-        user_agents.append(f"{me}{rand}")
+        user_agents.append(camouflage+rand)
     return user_agents
 
-def get_viewer_ids(vid, num):
+async def get_viewer_ids(vid, num):
     """
     This function extracts the viewer IDs.
 
@@ -93,55 +91,59 @@ def get_viewer_ids(vid, num):
             - str: The video ID extracted from the livestream URL.
             - str: The channel name.
     """
-    print("(+) Getting viewer ids...")
-    user_agents = generate_user_agent(num)
+    print("(+) Getting viewer ids, Please wait...")
     viewer_ids = {}
     url = f"https://rumble.com/embedJS/u3/?request=video&v={vid}"
- 
-    # since we only get the viewer ids once and re use them
-    # i don't want do this as fast as possible, if i see that its too slow i might improve it.
-    for user_agent in user_agents:
-        headers = {'User-Agent': user_agent}
-        try:
-            response = httpx.get(url, headers=headers)
-            data = response.json()
-            viewer_id = data.get('viewer_id')
-            video_id = data.get('vid')
-            channel = data["author"]["name"]
-            if viewer_id:
-                viewer_ids[viewer_id] = user_agent
-        except Exception as e:
-            print(f"something went wrong: {e}")
-    return viewer_ids, video_id, channel
 
-async def send_view(url, headers, body, viewer_id, verbose):
-    try:
-        async with httpx.AsyncClient() as client:
-            response = await client.post(url, headers=headers, data=body)
-            response.raise_for_status()
-            if verbose:
-                if response.status_code == 200:
-                    print(f"(~) id {viewer_id} was sent and accepted.")
-                else:
-                    print(f"(~) id {viewer_id} was rejected.")
-    except Exception as e:
-        print(f"Something went wrong {e}")
+    async with httpx.AsyncClient() as client:
+        for i in range(num):
+            user_agent = await generate_user_agent(1) # if it works, it works.
+            headers = {'User-Agent': user_agent[0]}
+            response = await client.get(url, headers=headers)
+            try:
+                data = response.json()
+                viewer_id = data.get('viewer_id')
+                video_id = data.get('vid')
+                channel = data["author"]["name"]
+                if viewer_id:
+                    viewer_ids[viewer_id] = user_agent[0]
+                print(f"\r(+) Retrieved {i+1}/{num}", end='', flush=True)
+            except Exception as e:
+                print(f"something went wrong: {e}")
+
+    print()
+    return viewer_ids, video_id, channel
 
 async def viewbot(viewer_ids, video_id, verbose):
     url = "https://wn0.rumble.com/service.php?api=7&name=video.watching-now"
 
-    tasks = []
-    for viewer_id, user_agent in viewer_ids.items():
-        headers = {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'User-Agent': user_agent
-        }
-        body = f"video_id={video_id}&viewer_id={viewer_id}"
-        tasks.append(send_view(url, headers, body, viewer_id, verbose))
+    async with httpx.AsyncClient() as client:
+        tasks = []
+        for viewer_id, user_agent in viewer_ids.items():
+            headers = {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'User-Agent': user_agent
+            }
+            body = f"video_id={video_id}&viewer_id={viewer_id}"
+            task = asyncio.create_task(send_view(client, url, headers, body, viewer_id, verbose))
+            tasks.append(task)
+        
+        await asyncio.gather(*tasks)
 
-    await asyncio.gather(*tasks)
+async def send_view(client, url, headers, body, viewer_id, verbose):
+    try:
+        response = await client.post(url, headers=headers, data=body)
+        response.raise_for_status()
+        if verbose:
+            if response.status_code == 200:
+                print(f"(~) id {viewer_id} was sent and accepted.")
+            else:
+                print(f"(~) id {viewer_id} was rejected.")
+    except Exception as e:
+        print(f'ops: {e}')
 
-def main():
+
+async def main():
     parser = argparse.ArgumentParser()
     parser = argparse.ArgumentParser(
                     prog='Rumbot',
@@ -164,22 +166,19 @@ def main():
     if not vid_id:
         print("wrong livestream link bud, try again.")
         return
-    viewer_ids, video_id, channel = get_viewer_ids(vid_id, bots)
+    viewer_ids, video_id, channel = await get_viewer_ids(vid_id, bots)
     
-    print(f"(+) Retrieved {bots} bots.")
     print(f"(+) view botting channel: {channel}")
     print("(~) Press Ctrl + C when your done to exit.")
     
     while True:
         try:
-
-            asyncio.run(viewbot(viewer_ids, video_id, verbose))
-            # viewbot(viewer_ids, video_id, verbose)
-            # we keep sending the same bots every 60 seconds
-            time.sleep(60)
+            await viewbot(viewer_ids, video_id, verbose)
+            await asyncio.sleep(60)
         except KeyboardInterrupt as e:
             return
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
+
 
